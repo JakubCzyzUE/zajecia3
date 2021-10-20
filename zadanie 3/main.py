e579f79df64dce202c80b358f1edd78d080e45f2
Jakub Cz <92373216+JakubCzyzUE@users.noreply.github.com>
2021-10-20 14:33:19 +0200

    zadanie 3 całe

---
+++ zadanie 3/main.py
@@ -0,0 +1,11 @@
+def isEven(x: int):
+
+    if(x % 2 == 0):
+        print("Liczba jest parzysta")
+        return True
+    else:
+        print("Liczba nie jest parzysta")
+        return False
+
+isEven(10)
+isEven(7)
